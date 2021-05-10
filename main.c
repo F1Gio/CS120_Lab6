734fabedb9a53ad9a1561d52c5b3891ec1c100c3
F1Gio <44180447+F1Gio@users.noreply.github.com>
2021-05-09 23:31:33 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,164 +1,226 @@
-/*	Author: Giovany Turrubiartes
+/*	Author: gturr001, Giovany Turrubiartes
  *  Partner(s) Name: 
- *	Lab Section: 022
- *	Assignment: Lab #6  Exercise #2
+ *	Lab Section:
+ *	Assignment: Lab #8  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
-volatile unsigned char TimerFlag = 0;
-unsigned long _avr_timer_M = 1;
-unsigned long _avr_timer_cntcurr = 0;
+enum SM1_States { SM1_Start, SM1_ON, SM1_OFF, SM1_Wait} SM1_State;
+enum SM2_States { SM2_Start, SM2_Up, SM2_Down, SM2_Wait} SM2_State;
+static unsigned button1 = 0x00;
+static unsigned button2 = 0x00;
+static unsigned button3 = 0x00;
 
-void TimerOn() {
-	TCCR1B = 0x0B;
-	OCR1A = 125;
-	TIMSK1 = 0x02;
-	TCNT1 = 0;
-	_avr_timer_cntcurr = _avr_timer_M;
-	SREG |= 0x80;
+void set_PWM(double frequency) {
+	static double current_frequency;
 
-}
+	if (frequency != current_frequency) {
+		if (!frequency) { TCCR3B &= 0x08; } 
+		else { TCCR3B |= 0x03; }
 
-void TimerOff() {
-	TCCR1B = 0x00;
-}
+		if (frequency < 0.954) {
+			OCR3A = 0xFFFF;
+		}
 
-void TimerISR() {
-	TimerFlag = 1;
-}
+		else if (frequency > 31250) {
+			OCR3A = 0x0000;
+		}
 
-ISR(TIMER1_COMPA_vect) {
-	_avr_timer_cntcurr--;
-	if (_avr_timer_cntcurr == 0) {
-		TimerISR();
-		_avr_timer_cntcurr = _avr_timer_M;
+		else {
+			OCR3A = (short)(8000000 / (128 * frequency)) - 1; 
+		}
+
+		TCNT3 = 0;
+		current_frequency = frequency;
+
 	}
 }
 
-void TimerSet(unsigned long M) {
-	_avr_timer_M = M;
-	_avr_timer_cntcurr = _avr_timer_M;
+void PWM_on () {
+	TCCR3A = (1 << COM3A0);
+
+	TCCR3B = (1 << WGM32);
+
+	set_PWM(0);
+
+}
+void PWM_off () {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
 }
 
-static unsigned char led = 0x00;
-static unsigned char buttonInc = 0x00;
-static unsigned char buttonDec = 0x00;
-static unsigned char Tot = 0x00;
-enum SM1_States {SM1_Start, SM1_Inc, SM1_Dec} SM1_State;
 
-void IncDec() {
-switch(SM1_State) {
+void OnOff() {
+	switch (SM1_State) { 
 		case SM1_Start:
-
-			if (buttonInc) {
-				SM1_State = SM1_Inc;
-			}
-
-			else if (buttonDec) {
-				SM1_State = SM1_Dec;
+			if (button1) {
+				SM1_State = SM1_ON;
 			}
 
 			else {
-				SM1_State = SM1_Start;
-			}
-			break;
-
-		case SM1_Inc:
-			if (buttonInc) {
-				SM1_State = SM1_Inc;
-			}
-
-			else if (buttonDec){
-				SM1_State = SM1_Dec;
-			}
-
-			else if (buttonInc && buttonDec) {
-				SM1_State = SM1_Start;
+				SM1_State = SM1_OFF;
 			}
 
 			break;
 
-		case SM1_Dec:
-			if (buttonDec) {
-				SM1_State = SM1_Dec;
+		case SM1_ON:
+			SM1_State = SM1_Wait;
+			break;
+
+		case SM1_OFF:
+			SM1_State = SM1_Start;
+			break;
+
+		case SM1_Wait:
+			if (button1) {
+				SM1_State = SM1_OFF;
 			}
 
-			else if (buttonInc){
-				SM1_State = SM1_Inc;
-			}
-
-			else if (buttonInc && buttonDec) {
-				SM1_State = SM1_Start;
+			else {
+				SM1_State = SM1_Wait;
 			}
 
 			break;
+	}
 
+	
+	switch(SM1_State) {
+		case SM1_Start:
+			PWM_off();
+			break;
+
+		case SM1_ON:
+			PWM_on();
+			break;
+
+		case SM1_OFF:
+			break;
+
+		case SM1_Wait:
+			PWM_off();
+			break;
+
+
+	}
 			
-	}
-
-	switch (SM1_State) {
-		case (SM1_Start):
-			Tot = 0;
-			led = (led & 0xF8) | 0x00;
-			PORTB = led;
-			break;
-
-		case (SM1_Inc):
-
-			if (Tot < 9) {
-				Tot = Tot + 1;
-				led = (led & 0xF0) | Tot;
-				PORTB = led;
-			}
-
-			break;
-
-		case (SM1_Dec):
-
-			if (Tot > 0) {
-				Tot = Tot - 1;
-				led = (led & 0xF0) | Tot;
-				PORTB = led;
-			}
-			break;
-	}
-		
-		
 
 }
+
+void UpDown() {
+	static double noteArray [8] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00,493.33, 523.25};
+	static int currVal = 0;
+
+	switch (SM2_State) {
+		case SM2_Start:
+			if (button2) {
+				SM2_State = SM2_Up;
+			}
+
+			else if (button3) {
+				SM2_State = SM2_Down;
+			}
+
+			else {
+				SM2_State = SM2_Start;
+			}
+
+			break;
+
+		case SM2_Up:
+
+			SM2_State = SM2_Wait;
+			break;
+
+		case SM2_Down:
+
+			SM2_State = SM2_Wait;
+			break;
+
+
+		case SM2_Wait:
+			if (button2) {
+				SM2_State = SM2_Up;
+			}
+
+			else if (button3) {
+				SM2_State = SM2_Down;
+			}
+
+			else {
+				SM2_State = SM2_Wait;
+			}
+			
+			break;
+
+		default:
+			SM2_State = SM2_Start;
+
+			break;
+
+	}
+
+
+	switch (SM2_State) {
+		case SM2_Start:
+//			currVal = 0;
+			break;
+
+		case SM2_Up:
+			if (currVal < 7) {
+				currVal = currVal + 1;
+//				set_PWM(noteArray[currVal]);
+
+			}
+
+
+			break;
+
+		case SM2_Down:
+			if (currVal > 1) {
+				currVal = currVal - 1;
+//				set_PWM(noteArray[currVal]);
+			}
+
+			break;
+
+		case SM2_Wait:
+
+			set_PWM(noteArray[currVal]);
+			break;
+
+
+	}
+			
+}
+
 
 
 int main(void) {
 	DDRA = 0x00;
 	PORTA = 0xFF;
-	DDRB = 0xFF;
+	DDRB = 0x40; 
 	PORTB = 0x00;
-	TimerSet(100);
-	TimerOn();
-//	unsigned char led = 0x00;
-//	unsigned char button = 0x00;
-    /* Insert DDR and PORT initializations */
 
+
+	PWM_on();
+		
     /* Insert your solution below */
     while (1) {
 
-	buttonInc = ~PINA & 0x01;
-	buttonDec = ~PINA & 0x02;
-	IncDec();
-//	PORTB = led;
-	while (!TimerFlag);
-	TimerFlag = 0;
+	    button1 = ~PINA & 0x01;
+	    button2 = ~PINA & 0x02;
+	    button3 = ~PINA & 0x04;
 
-
+//	    OnOff();
+	    UpDown();
+	    
 
     }
-
 }
